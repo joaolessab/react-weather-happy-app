9797f0d83645ee374563bb1221c3f93b951ac8e7
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-10-27 00:50:57 -0300

    [App] - Generating fixed values before API gets called

--- weather-happy-app/src/App.js
+++ weather-happy-app/src/App.js
@@ -6,15 +6,45 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    let days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  };
+
   return (
     <div className='app'>
       <main>
+        {/* Search Section */}
         <div className='search-box'>
           <input 
             type='text'
             className='search-bar'
             placeholder='Pesquisar local...'
           />
+        </div>
+
+        {/* Location Section */}
+        <div>
+          <div className='location-box'>
+            <div className='location'>Paraibuna, SP</div>
+            <div className='date'>{ dateBuilder(new Date()) }</div>
+          </div>
+        </div>
+
+        {/* Weather Section */}
+        <div className='weather-box'>
+          <div className='temp'>
+            22° C
+          </div>
+          <div className='weather'>Ensolarado</div>
         </div>
       </main>
     </div>
