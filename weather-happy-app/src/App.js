3dec792b77edfb662730068df1bd12ce0929f5bb
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-10-27 00:35:26 -0300

    [App] - Basic Structure

--- weather-happy-app/src/App.js
+++ weather-happy-app/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React from 'react';
+
+const api = {
+  key: 'd3c5928599c5c22e8bc6dd88ca81706d',
+  base: 'http://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
   return (
-    <div className="App">
-      <p>Clean App</p>
+    <div className='app'>
+      <main>
+        <div className='search-box'>
+          <input 
+            type='text'
+            className='search-bar'
+            placeholder='Pesquisar local...'
+          />
+        </div>
+      </main>
     </div>
   );
 }
